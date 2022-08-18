2c5dd6131ece409d3e11dfde0d5bd615ced86f9b
unknown <sailalith1993@gmail.com>
2022-08-18 14:12:27 +0530

    refresing the component every 2 seconds when in inbox to retrieve mails instantly

--- src/components/Mail/Inbox.js
+++ src/components/Mail/Inbox.js
@@ -11,70 +11,73 @@ const Inbox = () => {
   //   const navigate = useNavigate();
   const [deleted, setDeleted] = useState(false);
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(
-        `https://my-chat-app-2b721-default-rtdb.firebaseio.com/${parentMailEndPoint}/inbox.json`
-      );
-      const data = await response.json();
-      console.log(data);
-      const loadedData = [];
-
-      for (const key in data) {
-        loadedData.push({
-          id: key,
-          toEmail: data[key].toEmail,
-          fromEmail: data[key].fromEmail,
-          text: data[key].text,
-          sub: data[key].sub,
-          key: key,
-          seen: data[key].seen,
-        });
-      }
-      let unseen = 0;
-
-      const deleteMailHandler = (id) => {
-        setDeleted(false);
-        fetch(
-          `https://my-chat-app-2b721-default-rtdb.firebaseio.com/${parentMailEndPoint}/inbox/${id}.json`,
-          {
-            method: "DELETE",
-          }
-        )
-          .then((res) => {
-            if (res.ok) {
-              return res.json();
-            }
-          })
-          .then((data) => {
-            console.log(data);
-            setDeleted(true);
-            console.log(deleted);
-            // navigate("/");
-          })
-          .catch((err) => {
-            throw new Error(err.message);
-          });
-      };
-      const emailList = loadedData.map((item) => {
-        if (item.seen === false) {
-          unseen++;
-        }
-        return (
-          <InboxItem
-            id={item.id}
-            toEmail={item.toEmail}
-            fromEmail={item.fromEmail}
-            sub={item.sub}
-            text={item.text}
-            seen={item.seen}
-            onDelete={deleteMailHandler.bind(null, item.id)}
-          />
+    const intervalId = setInterval(() => {
+      const fetchData = async () => {
+        const response = await fetch(
+          `https://my-chat-app-2b721-default-rtdb.firebaseio.com/${parentMailEndPoint}/inbox.json`
         );
-      });
-      setUnseenMail(unseen);
-      setReceivedEmails(emailList);
-    };
-    fetchData();
+        const data = await response.json();
+        console.log(data);
+        const loadedData = [];
+
+        for (const key in data) {
+          loadedData.push({
+            id: key,
+            toEmail: data[key].toEmail,
+            fromEmail: data[key].fromEmail,
+            text: data[key].text,
+            sub: data[key].sub,
+            key: key,
+            seen: data[key].seen,
+          });
+        }
+        let unseen = 0;
+
+        const deleteMailHandler = (id) => {
+          setDeleted(false);
+          fetch(
+            `https://my-chat-app-2b721-default-rtdb.firebaseio.com/${parentMailEndPoint}/inbox/${id}.json`,
+            {
+              method: "DELETE",
+            }
+          )
+            .then((res) => {
+              if (res.ok) {
+                return res.json();
+              }
+            })
+            .then((data) => {
+              console.log(data);
+              setDeleted(true);
+              console.log(deleted);
+              // navigate("/");
+            })
+            .catch((err) => {
+              throw new Error(err.message);
+            });
+        };
+        const emailList = loadedData.map((item) => {
+          if (item.seen === false) {
+            unseen++;
+          }
+          return (
+            <InboxItem
+              id={item.id}
+              toEmail={item.toEmail}
+              fromEmail={item.fromEmail}
+              sub={item.sub}
+              text={item.text}
+              seen={item.seen}
+              onDelete={deleteMailHandler.bind(null, item.id)}
+            />
+          );
+        });
+        setUnseenMail(unseen);
+        setReceivedEmails(emailList);
+      };
+      fetchData();
+    }, 2000);
+    return () => clearInterval(intervalId);
   }, [parentMailEndPoint, deleted]);
   return (
     <div>
